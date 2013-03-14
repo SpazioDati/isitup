73d6b49e3caa55750c726322f98b31305e5fc029
Davide Setti <davide.setti@gmail.com>
2013-03-14 22:27:35 +0100

    import project

---
+++ isitup.py
@@ -0,0 +1,52 @@
+import urllib2
+from contextlib import contextmanager
+import uwsgi
+from bottle import get, default_app, request, abort, response
+
+EXPIRE = 60
+
+
+def valid(url):
+    return url
+
+
+def invalid():
+    response.status = 400
+    return 'URL is down'
+
+
+@contextmanager
+def lock(lock_idx):
+    uwsgi.lock(lock_idx)
+    yield
+    uwsgi.unlock(lock_idx)
+
+
+@get('/')
+def index():
+    try:
+        url = request.params['url']
+    except KeyError:
+        abort(400, 'Please provide a url')
+
+    url_hash = hash(url)
+    cache_key = str(url_hash)
+
+    lock_idx = url_hash % 24
+    with lock(lock_idx):
+        cache = uwsgi.cache_get(cache_key)
+        if cache == 'd':
+            return invalid()
+        if cache == 'u':
+            return valid(url)
+
+        try:
+            urllib2.urlopen(url, timeout=100)
+        except:
+            uwsgi.cache_set(cache_key, 'd', EXPIRE)
+            return invalid()
+        else:
+            uwsgi.cache_set(cache_key, 'u', EXPIRE)
+            return valid(url)
+
+application = default_app()
